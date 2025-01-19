254e6648652a4f6c8f9a15809bc2daab4eb6ecf6
TomAnKing <71315446+TomAnKing@users.noreply.github.com>
2025-01-19 19:31:30 +0100

    added login page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      primaryBlue: "#2B3D67",
+      secondaryBlue: "#D5FCF2",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
